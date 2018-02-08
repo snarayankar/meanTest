df2622dbbd64d08334eabef4f8ac032b4da8ab85
Santosh Narayankar <snarayankar@aamc.org>
2018-02-08 11:28:39 -0500

    DEV000: Node first commit

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl : 'mongodb://node:foodviolators@foodviolators.cx22urjmcic4.us-east-1.rds.amazonaws.com:3306/foodViolators',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
